5aa7af9df2c999f018c13f5061e7639357624a7c
Aravind <aravindmhack19@gmail.com>
2015-09-24 19:36:16 +0530

    File uploading

---
+++ client.py
@@ -0,0 +1,47 @@
+import socket
+import sys, string, select
+ 
+HOST = '192.168.1.16'   
+PORT = 8888 
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+address = (HOST,PORT)
+s.connect(address)
+print "connecting..."
+s.send("start")
+while 1:
+    filename = s.recv(1024)
+    if filename == 'DownloadComplete':
+        print "--->>>>>>> DownloadComplete <<<<<<<<<<----"
+        userdata=raw_input()
+        break      
+    s.send("got it")
+    data = s.recv(1024)
+    if data[:5] == 'Exist':
+        filesize = long(data[5:])
+        message = raw_input(filename + " file exists. " + str(filesize) + "bytes..download? (Y/N)? ->")
+        if message =='Y' or message =='y':
+            s.send("OK")
+            f = open("new_" + filename, 'wb')
+            data = s.recv(1024)
+            totalRecv = len(data)
+            f.write(data)
+            while totalRecv < filesize:
+                data = s.recv(1024)
+                totalRecv += len(data)
+                f.write(data)
+                print "{0:.2f}".format((totalRecv/float(filesize))*100)+"Done"
+            s.send("complete")
+        else:
+            break
+
+    else:
+        print "File doesn't exist"
+
+                            		
+
+
+	
+ 
+
+
---
+++ server.py
@@ -0,0 +1,65 @@
+import socket
+import threading
+import os
+from os.path import isfile, join,isdir
+path='/home/aravind/'
+
+
+def Retfile(name,sock):
+    files = []
+    fi = search_file(path,files)
+    for filename in fi:
+        if os.path.getsize(filename) != 0:
+            data = sock.recv(1024)
+            if os.path.isfile(filename):
+                client_file = filename.split('/')[-1]
+                sock.send(client_file)
+                sock.recv(1024)
+                sock.send("Exist" + str(os.path.getsize(filename)))
+                userRes = sock.recv(1024)
+                if userRes[:2] == 'OK':
+                    with open(filename,'rb') as f:
+                        bytesToSend = f.read(1024)
+                        sock.send(bytesToSend)
+                        while bytesToSend != '':
+                            bytesToSend = f.read(1024)
+                            sock.send(bytesToSend)
+            else:
+                pass
+    data = sock.recv(1024)  
+    if data =='complete':
+        sock.send("DownloadComplete")
+
+def search_file(path,files):
+    if isdir(path):
+        print "searching ...."
+        for f in os.listdir(path):
+            if isfile(join(path, f)):
+                path_f = path+f
+                files.append(path_f)
+            else:
+                temp_path = join(path, f) + '/'
+                temp_file = search_file(temp_path,files)
+    return files    
+
+
+HOST = '192.168.1.16'   
+PORT = 8888 
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(10)
+print "listening"
+
+
+while 1:
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    t=threading.Thread(target=Retfile,args=("Thread1",conn))
+    t.start()
+
+       
+
+        
+
+
