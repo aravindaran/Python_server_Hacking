bcb316504cfe4ad71b6f7a7062d1a5cc24eb9fc7
Aravind <aravindmhack19@gmail.com>
2015-09-24 19:38:08 +0530

    Server update

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ import socket
 import threading
 import os
 from os.path import isfile, join,isdir
-path='/home/aravind/'
 
+path='/home/aravind/'
 
 def Retfile(name,sock):
     files = []
@@ -42,16 +42,12 @@ def search_file(path,files):
                 temp_file = search_file(temp_path,files)
     return files    
 
-
 HOST = '192.168.1.16'   
 PORT = 8888 
- 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(10)
 print "listening"
-
-
 while 1:
     conn, addr = s.accept()
     print 'Connected with ' + addr[0] + ':' + str(addr[1])
